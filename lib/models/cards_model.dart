6303091a5ebefc0e170b622dcfdd3d262a95f86a
ErdoganBozkurt <bozkurt.erdogan@outlook.com>
2023-05-17 02:03:59 +0300

    feat: Game page ui done

--- lib/models/cards_model.dart
+++ lib/models/cards_model.dart
@@ -1,11 +1,13 @@
 class CardData {
   int number;
   int location;
+  bool isVisible;
 
-  CardData({required this.number, required this.location});
+  CardData({required this.number, required this.location, this.isVisible = true});
 
   @override
   String toString() {
-    return 'CardData{number: $number, location: $location}';
+    return 'CardData(number: $number, location: $location, isVisible: $isVisible)';
   }
 }
+
--- lib/pages/game_page_view.dart
+++ lib/pages/game_page_view.dart
@@ -6,26 +6,28 @@ import 'package:flutter_memory_game/constants/app_colors.dart';
 import '../models/cards_model.dart';
 
 class GamePage extends StatefulWidget {
-  const GamePage({super.key});
+  const GamePage({Key? key}) : super(key: key);
 
   @override
-  State<GamePage> createState() => _GamePageState();
+  _GamePageState createState() => _GamePageState();
 }
 
 class _GamePageState extends State<GamePage> {
   List<CardData> cards = [];
   int correctCards = 4;
   int maxCards = 40;
+  int maxLives = 3;
   int lives = 3;
   List<int> locations = [];
+  int nextCardNumber = 1;
+  bool isCorrectCardTapped = false;
 
   @override
   void initState() {
     super.initState();
-    cards = generateCards();
+    resetGame();
+    
   }
-
-  //TODO: Create a function to generate random locations for the cards.
 
   @override
   Widget build(BuildContext context) {
@@ -38,86 +40,148 @@ class _GamePageState extends State<GamePage> {
           'Memory Game',
           style: Theme.of(context)
               .textTheme
-              .titleLarge
+              .headline6
               ?.copyWith(color: Colors.deepPurple.shade800),
         ),
         centerTitle: true,
       ),
-      body: //A 5x8 gridview. and the cards are randomly generated in the gridview.
-          buildCardGrid(),
+      body: buildCardGrid(),
     );
   }
 
-  void onCardTapped(int index) {}
+  void onCardTapped(int cardNumber) {
+  bool isCorrectCardTapped = false;
+  int tappedCardIndex = -1;
 
-  List<CardData> generateCards() {
-  List<CardData> cards = [];
-  int cardCount = 4;
-  locations = generateRandomCardLocations(5 * 8, cardCount);
-
-  for (int i = 0; i < cardCount; i++) {
-    int location = locations[i];
-    CardData card = CardData(number: i + 1, location: location);
-    cards.add(card);
-  }
-  print(cards.toString());
-  return cards;
-}
-
-
- List<int> generateRandomCardLocations(int gridSize, int cardCount) {
-  List<int> cardLocations = [];
-  Set<int> usedLocations = {};
-
-  while (cardLocations.length < cardCount) {
-    int randomLocation = Random().nextInt(gridSize);
-    if (!usedLocations.contains(randomLocation)) {
-      cardLocations.add(randomLocation);
-      usedLocations.add(randomLocation);
-    } else {
-      continue; // Aynı lokasyon tekrarlandığı için döngüyü devam ettir
+  for (int i = 0; i < cards.length; i++) {
+    if (cards[i].number == cardNumber && cards[i].isVisible) {
+      isCorrectCardTapped = true;
+      tappedCardIndex = i;
+      break;
     }
   }
 
-  return cardLocations;
+  if (isCorrectCardTapped) {
+    cards[tappedCardIndex].isVisible = false;
+    int correctCardCount = cards.where((card) => !card.isVisible).length;
+    if (correctCardCount == cards.length) {
+      correctCards++;
+      resetGame();
+    }
+  } else {
+    lives--;
+    if (lives <= 0) {
+      showGameOverPopup();
+    } else {
+      resetVisibleState();
+    }
+  }
+
+  setState(() {});
+}
+
+void resetVisibleState() {
+  for (int i = 0; i < cards.length; i++) {
+    cards[i].isVisible = true;
+  }
 }
 
 
 
+
+  void showGameOverPopup() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('Game Over'),
+        content: const Text('You have lost the game.'),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              resetGame();
+              Navigator.pop(context);
+            },
+            child: const Text('Restart'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void resetGame() {
+    nextCardNumber = 1;
+    lives = maxLives;
+    cards = generateCards();
+  }
+
+  List<CardData> generateCards() {
+    List<CardData> cards = [];
+    int cardCount = correctCards;
+    locations = generateRandomCardLocations(8 * 5, cardCount);
+
+    for (int i = 0; i < cardCount; i++) {
+      int location = locations[i];
+      CardData card =
+          CardData(number: i + 1, location: location, isVisible: true);
+      cards.add(card);
+    }
+
+    return cards;
+  }
+
+  List<int> generateRandomCardLocations(int gridSize, int cardCount) {
+    List<int> cardLocations = [];
+    Set<int> usedLocations = {};
+
+    while (cardLocations.length < cardCount) {
+      int randomLocation = Random().nextInt(gridSize);
+      if (!usedLocations.contains(randomLocation)) {
+        cardLocations.add(randomLocation);
+        usedLocations.add(randomLocation);
+      }
+    }
+
+    return cardLocations;
+  }
+
   Widget buildCardGrid() {
     return GridView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: 8 * 5,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 8, // Sütun sayısı
+        crossAxisCount: 5,
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
       ),
-      itemCount: 5 * 8, // Grid hücre sayısı
       itemBuilder: (context, index) {
-        CardData matchingCard = cards.firstWhere(
-          (card) => card.location == index,
-          orElse: () =>
-              CardData(number: -1, location: -1), // Dummy kart oluştur
-        );
+        CardData card = cards.firstWhere((card) => card.location == index,
+            orElse: () => CardData(number: 0, location: -1, isVisible: false));
 
-        if (matchingCard.number != -1) {
-          return buildCardView(matchingCard.number);
-        } else {
-          return buildEmptyContainer();
-        }
+        return GestureDetector(
+          onTap: () => onCardTapped(card.number),
+          child: Container(
+            decoration: BoxDecoration(
+              color: card.isVisible ? Colors.white : Colors.transparent,
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(
+                color: card.isVisible ? Colors.transparent : Colors.white,
+                width: 2,
+              ),
+            ),
+            child: Center(
+              child: Text(
+                card.isVisible ? '${card.number}' : '',
+                style: Theme.of(context).textTheme.headline6?.copyWith(
+                      color: card.isVisible
+                          ? Colors.deepPurple.shade800
+                          : Colors.white,
+                    ),
+              ),
+            ),
+          ),
+        );
       },
     );
-  }
-
-  Widget buildCardView(int cardNumber) {
-    return Card(
-      child: Center(
-        child: Text(
-          cardNumber.toString(),
-          style: const TextStyle(fontSize: 16),
-        ),
-      ),
-    );
-  }
-
-  Widget buildEmptyContainer() {
-    return Container();
   }
 }
